7b06f3e0e83e48f7c05b0057eabcd25db04452e8
mildtamon <mildtamon@gmail.com>
2023-03-21 16:42:23 +0700

    matrix_trace

--- src/matrix_multiply_dot/mod.rs
+++ src/matrix_multiply_dot/mod.rs
@@ -1,26 +1,35 @@
 use rayon::iter::*;
 
 // dot product and matrix multiply
-fn par_dot_product(x: &[f64], y: &[f64]) -> f64 {
+fn dot_product(x: &[f64], y: &[f64]) -> f64 {
     x.par_iter().zip(y).map(|(&xk, &yk)| xk * yk).sum()
 }
 
 fn matrix_mult(m1: Vec<Vec<i32>>, m2: Vec<Vec<i32>>) -> f64 {
     // TODO: implement parallel matrix multiply
+
     panic!("not implemented yet!");
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{par_dot_product, matrix_mult};
+    use std::iter::zip;
+    use super::{dot_product, matrix_mult};
 
     #[test]
     fn dot_test() {
-        // TODO: implement test cases -> check empty vector, different size
+        // TODO: implement test cases
+        assert_eq!(0.0, dot_product(&vec![], &vec![]));
+        assert_eq!(0.0, dot_product(&vec![0.0], &vec![0.0]));
+        assert_eq!(14.0, dot_product(&vec![1.0, 2.0, 3.0], &vec![1.0, 2.0, 3.0]));
     }
 
     #[test]
     fn mult_test() {
         // TODO: implement test cases -> error when size not match
+        let itr1 = vec![1, 2, 3, 4];
+        let itr2 = vec![11, 12, 13 , 14];
+        let itr3 = vec![21, 22, 23, 24];
+
     }
 }
--- src/matrix_trace/mod.rs
+++ src/matrix_trace/mod.rs
@@ -1,7 +1,10 @@
-// trace
-fn trace(m: Vec<Vec<f64>>) -> f64 {
-    // TODO: implement parallel matrix trace
-    panic!("not implemented yet!");
+use rayon::iter::*;
+
+fn trace(m: &Vec<Vec<f64>>) -> (f64) {
+    // sequential matrix trace is better
+    let mut sum: f64 = 0.0;
+    for index in 0..m.len() { sum += m[index][index] }
+    sum
 }
 
 #[cfg(test)]
@@ -10,6 +13,10 @@ mod tests {
 
     #[test]
     fn trace_test() {
-        // TODO: implement test cases
+        // TODO: implement more test cases
+        assert_eq!(15.0, trace(&vec![vec![1.0 ,2.0 ,3.0],
+                                        vec![4.0 ,5.0 ,6.0],
+                                        vec![7.0 ,8.0 ,9.0]]));
+        assert_eq!(0.0, trace(&vec![]));
     }
 }
