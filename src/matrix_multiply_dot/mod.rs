fcbe8d5c246e60ee2674bd518a2c8f85eca7bdfd
pkrittin <61532699+pkrittin@users.noreply.github.com>
2023-04-02 22:11:33 +0700

    done par_matrix_mult

--- src/matrix_multiply_dot/mod.rs
+++ src/matrix_multiply_dot/mod.rs
@@ -10,7 +10,7 @@ fn seq_dot_product(x: &Vec<f64>, y: &Vec<f64>) -> f64 {
     x.iter().zip(y).map(|(&xk, &yk)| xk * yk).sum()
 }
 
-fn matrix_mult(m1: Vec<Vec<f64>>, m2: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+fn seq_matrix_mult(m1: Vec<Vec<f64>>, m2: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     let mut ans = vec![vec![0f64; m2[0].len()]; m1.len()];
     let m2t: Vec<Vec<f64>> = matrix_transpose::par_transpose(m2.clone());
 
@@ -22,10 +22,23 @@ fn matrix_mult(m1: Vec<Vec<f64>>, m2: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     ans
 }
 
+fn par_matrix_mult(m1: Vec<Vec<f64>>, m2: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+    let m2t: Vec<Vec<f64>> = matrix_transpose::par_transpose(m2.clone());
+    m1.par_iter()
+        .map(|r| {
+            (0..m2t.len())
+                .into_par_iter()
+                .map(|c| par_dot_product(r, &m2t[c]))
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
+}
+
+
 #[cfg(test)]
 mod tests {
     use std::iter::zip;
-    use super::{par_dot_product, seq_dot_product, matrix_mult};
+    use super::{par_dot_product, seq_dot_product, par_matrix_mult, seq_matrix_mult};
     use std::time::{Duration, Instant};
 
     fn timed<R, F>(f: F) -> (R, Duration) where F: Fn() -> R {
@@ -65,12 +78,33 @@ mod tests {
     #[test]
     fn mult_test() {
         // TODO: implement test cases -> error when size not match
-        println!("{:?}", matrix_mult(vec![vec![1.0, 2.0, 3.0],
+        assert_eq!(vec![vec![30.0, 36.0, 42.0],
+                      vec![66.0, 81.0, 96.0],
+                      vec![102.0, 126.0, 150.0],
+                      vec![138.0, 171.0, 204.0]],
+                 seq_matrix_mult(vec![vec![1.0, 2.0, 3.0],
                                                 vec![4.0, 5.0, 6.0],
                                                 vec![7.0, 8.0, 9.0],
                                                 vec![10.0, 11.0, 12.0]],
                                      vec![vec![1.0, 2.0, 3.0],
                                                 vec![4.0, 5.0, 6.0],
                                                 vec![7.0, 8.0, 9.0]]));
+        assert_eq!(vec![vec![30.0, 36.0, 42.0],
+                        vec![66.0, 81.0, 96.0],
+                        vec![102.0, 126.0, 150.0],
+                        vec![138.0, 171.0, 204.0]],
+                   par_matrix_mult(vec![vec![1.0, 2.0, 3.0],
+                                        vec![4.0, 5.0, 6.0],
+                                        vec![7.0, 8.0, 9.0],
+                                        vec![10.0, 11.0, 12.0]],
+                                   vec![vec![1.0, 2.0, 3.0],
+                                        vec![4.0, 5.0, 6.0],
+                                        vec![7.0, 8.0, 9.0]]));
+
+        //pls complete it thx
+        let a_seq: Vec<Vec<f64>> = (0..=1024).map(|a| (0..=a).map(|b| b as f64).collect()).collect();
+        let (output, time) = timed(|| seq_matrix_mult(a_seq.clone(), a_seq.clone()));
+        println!("sequential with 1024-size time: {:?} output: {:?}", time, output);
     }
+
 }
