047dff46bc5a90973175b76ca6cf553cb0cc5153
mildtamon <mildtamon@gmail.com>
2023-04-02 23:13:11 +0700

    fixing test and format

--- src/matrix_trace/mod.rs
+++ src/matrix_trace/mod.rs
@@ -1,14 +1,12 @@
 use rayon::iter::*;
 
-fn seq_trace(m: &Vec<Vec<f64>>) -> (f64) {
+fn seq_trace(m: &Vec<Vec<f64>>) -> f64 {
     // sequential matrix trace is better (in smaller matrix)
     (0..m.len()).into_iter().map(|index| m[index][index]).sum()
 }
 
 fn par_trace(m: &Vec<Vec<f64>>) -> (f64) {
-    let mut sum = 0.0;
-    (0..m.len()).into_iter().for_each(|index| sum += m[index][index]);
-    sum
+    (0..m.len()).into_par_iter().map(|r| m[r][r]).sum()
 }
 
 #[cfg(test)]
@@ -24,22 +22,26 @@ mod tests {
 
     #[test]
     fn trace_test() {
-        // TODO: implement more test cases
-        assert_eq!(15.0, seq_trace(&vec![vec![1.0 ,2.0 ,3.0],
-                                        vec![4.0 ,5.0 ,6.0],
-                                        vec![7.0 ,8.0 ,9.0]]));
-        assert_eq!(0.0, seq_trace(&vec![]));
-        assert_eq!(15.0, par_trace(&vec![vec![1.0 ,2.0 ,3.0],
-                                     vec![4.0 ,5.0 ,6.0],
-                                     vec![7.0 ,8.0 ,9.0]]));
-        assert_eq!(0.0, par_trace(&vec![]));
-
+        let small_matrix = vec![vec![1.0 ,2.0 ,3.0],
+                                                vec![4.0 ,5.0 ,6.0],
+                                                vec![7.0 ,8.0 ,9.0]];
         let two_d_matrix = vec![(0..=1024).map(|a| a as f64).collect::<Vec<_>>(); 1024];
 
-        let (output, time) = timed(|| seq_trace(&two_d_matrix.clone()));
+        assert_eq!(15.0, seq_trace(&small_matrix));
+        assert_eq!(0.0, seq_trace(&vec![]));
+        assert_eq!(15.0, par_trace(&small_matrix));
+        assert_eq!(0.0, par_trace(&vec![]));
+
+        let (output, time) = timed(|| seq_trace(&small_matrix));
+        println!("sequential matrix trace with small matrix   time: {:?}  output: {:?}", time, output);
+
+        let (output, time) = timed(|| par_trace(&small_matrix));
+        println!("parallel matrix trace with small matrix     time: {:?}  output: {:?}", time, output);
+
+        let (output, time) = timed(|| seq_trace(&two_d_matrix));
         println!("sequential matrix trace with 1024-size   time: {:?}  output: {:?}", time, output);
 
-        let (output, time) = timed(|| par_trace(&two_d_matrix.clone()));
+        let (output, time) = timed(|| par_trace(&two_d_matrix));
         println!("parallel matrix trace with 1024-size     time: {:?}  output: {:?}", time, output);
     }
 }
