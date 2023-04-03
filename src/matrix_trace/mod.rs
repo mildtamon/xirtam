ae462d91a8902b6f170ea3219257d3f78c6c2679
mildtamon <mildtamon@gmail.com>
2023-04-03 21:38:16 +0700

    update test

--- src/matrix_trace/mod.rs
+++ src/matrix_trace/mod.rs
@@ -22,21 +22,22 @@ mod tests {
 
     #[test]
     fn trace_test() {
-        let small_matrix = vec![vec![1.0 ,2.0 ,3.0],
+        let test = vec![vec![1.0 ,2.0 ,3.0],
                                 vec![4.0 ,5.0 ,6.0],
                                 vec![7.0 ,8.0 ,9.0]];
+        let small_matrix = vec![(0..=700).map(|a| a as f64).collect::<Vec<_>>(); 700];
         let two_d_matrix = vec![(0..=1024).map(|a| a as f64).collect::<Vec<_>>(); 1024];
 
-        assert_eq!(15.0, seq_trace(&small_matrix));
+        assert_eq!(15.0, seq_trace(&test));
         assert_eq!(0.0, seq_trace(&vec![]));
-        assert_eq!(15.0, par_trace(&small_matrix));
+        assert_eq!(15.0, par_trace(&test));
         assert_eq!(0.0, par_trace(&vec![]));
 
         let (output, time) = timed(|| seq_trace(&small_matrix));
-        println!("sequential matrix trace with small matrix   time: {:?}  output: {:?}", time, output);
+        println!("sequential matrix trace with 700-size   time: {:?}  output: {:?}", time, output);
 
         let (output, time) = timed(|| par_trace(&small_matrix));
-        println!("parallel matrix trace with small matrix     time: {:?}  output: {:?}", time, output);
+        println!("parallel matrix trace with 700-size     time: {:?}  output: {:?}", time, output);
 
         let (output, time) = timed(|| seq_trace(&two_d_matrix));
         println!("sequential matrix trace with 1024-size   time: {:?}  output: {:?}", time, output);
